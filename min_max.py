a8998ea277715f588f765f021206a143161adc44
afthab764 <afthab764@gmail.com>
2024-03-31 11:11:28 +0500

    add new max func

--- min_max.py
+++ min_max.py
@@ -1,12 +1,12 @@
 def main():
     list1=list(map(int,input('enter the list elements').split()))
     print(list1)
-    print(minimum(list1))
-def minimum(a):
-    min=a[0]
+    print("the max element is ; ",maximum(list1))
+def maximum(a):
+    max=a[0]
     for i in range(1,len(a)):
         
-        if min>a[i]:
-          min= a[i]
-    return min          
+        if max<a[i]:
+          max= a[i]
+    return max          
 main()
