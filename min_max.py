7cefeb6b1ef51a6e8595d16701f374e51b1fe8db
afthab764 <afthab764@gmail.com>
2024-03-31 10:50:50 +0500

    add init files

---
+++ min_max.py
@@ -0,0 +1,12 @@
+def main():
+    list1=list(map(int,input('enter the list elements').split()))
+    print(list1)
+    print(minimum(list1))
+def minimum(a):
+    min=a[0]
+    for i in range(1,len(a)):
+        
+        if min>a[i]:
+          min= a[i]
+    return min          
+main()
